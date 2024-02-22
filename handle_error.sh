7ec0964f209d071b9cf2a905ef7450b6270f0e8f
Esther Alo <esther.alo@digital.cabinet-office.gov.uk>
2024-02-22 11:01:20 +0000

    removed error script and corrected secret path

--- handle_error.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Your error-handling logic goes here
-if grep -q "FileNotFoundError" error.log; then
-    echo "[ERROR] FileNotFoundError occurred. Failing the workflow."
-    exit 1
-fi
