0882c6ca524c16c4222ae79e8f30203d2b1d0cc8
melvabout <andrew.melvin@digital.cabinet-office.gov.uk>
2024-03-01 11:47:40 +0000

    Added missing Sechub conformance to the terraform.

--- infrastructure/build/terraform/deployments/service-catalog-product-tests/pipeline-dependencies/artifacts.tf
+++ infrastructure/build/terraform/deployments/service-catalog-product-tests/pipeline-dependencies/artifacts.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_object_lock_configuration" "artifacts" {
 
   rule {
     default_retention {
-      mode = "COMPLIANCE"
+      mode = "GOVERNANCE"
       days = 90
     }
   }
@@ -56,6 +56,27 @@ resource "aws_s3_object" "empty_lambda" {
   key    = "python/empty-lambda.zip"
   source = data.local_file.empty_lambda_zip.filename
 
-  object_lock_mode              = "COMPLIANCE"
+  object_lock_mode              = "COMPLIANCE" # Change to GOVERNANCE once past 2025-02-19T12:00:00
   object_lock_retain_until_date = "2025-02-19T12:00:00Z"
+}
+
+resource "aws_s3_bucket_policy" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+  policy = data.aws_iam_policy_document.artifacts.json
+}
+
+resource "aws_cloudformation_stack" "artifacts_kms_key" {
+  name          = "artifacts-kms-key"
+  template_body = file("${path.module}/../../../../../../service_catalog_portfolios/encryption/kms/template.yaml")
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_cloudformation_stack.artifacts_kms_key.outputs.KeyId
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
--- infrastructure/build/terraform/deployments/service-catalog-product-tests/pipeline-dependencies/data.tf
+++ infrastructure/build/terraform/deployments/service-catalog-product-tests/pipeline-dependencies/data.tf
@@ -10,3 +10,32 @@ data "aws_iam_policy_document" "assume_role" {
     actions = ["sts:AssumeRole"]
   }
 }
+
+data "aws_iam_policy_document" "artifacts" {
+  
+  statement {
+    sid    = "AllowSSLRequestsOnly"
+
+    principals {
+      type = "*" 
+      identifiers = ["*"]
+    }
+
+    effect = "Deny"
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      aws_s3_bucket.artifacts.arn,
+      "${aws_s3_bucket.artifacts.arn}/*"
+    ]
+
+    condition {
+      variable = "aws:SecureTransport"
+      values = ["false"]
+      test = "Bool"
+    }
+  }
+}
